76bbf1304cd228151ee30530bccb96507b7298c0
brandon castillo <brancastillo32@gmail.com>
2021-10-14 21:05:06 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import react from 'react';
+const api = {
+  key: 'f83e57daec7ef7bf8e8d21a9585226c2',
+  base: 'https://api.openweathermap.org/data/2.5/'
+};
 
 function App() {
-  return <div className='App'>
-    creating weather app 
-  </div>;
+  return (
+    <div className='app'>
+      <main>
+        <div className='search-box'>
+          <input type='text' className='search-bar' placeholder='Location' />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
